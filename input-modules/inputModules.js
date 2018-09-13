e1a67d323353e3657d222b79339605f3297e8427
Lev-Stambler <levstamb@gmail.com>
2018-09-12 20:11:20 -0400

    began input library work

---
+++ input-modules/inputModules.js
@@ -0,0 +1,5 @@
+class inputModule extends module {
+    constructor() {
+        super('inputModule', `<input accept = "<{accept}>" align = "<{}>" alt = "<{}>" autocomplete = "<{}>" autofocus = "<{}>" checked = "<{}>" dirname = "<{}>" `)
+    }
+}
